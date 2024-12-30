0f913a74d4178a56ce2acecbeb7e4b64fb6a6d2d
Bug1642 <hasancan1642@hotmail.com>
2024-12-30 14:49:21 +0300

    First Commit

--- obj/Release/net9.0/Manipulation.AssemblyInfo.cs
+++ obj/Release/net9.0/Manipulation.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Manipulation")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d6b3311bee59bab8f12b0a537ec3c4dc20016de")]
 [assembly: System.Reflection.AssemblyProductAttribute("Manipulation")]
